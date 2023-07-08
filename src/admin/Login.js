782e860fbd176f0648769678cb685c6249473ace
saadiarif277 <76664316+saadiarif277@users.noreply.github.com>
2023-07-08 10:29:03 -0700

    Update Login.js

--- src/admin/Login.js
+++ src/admin/Login.js
@@ -283,7 +283,7 @@ export const Login = () => {
                           cy="12"
                           r="10"
                           stroke="currentColor"
-                          stroke-width="4"
+                          strokeWidth="4"
                         ></circle>
                         <path
                           className="opacity-75"
